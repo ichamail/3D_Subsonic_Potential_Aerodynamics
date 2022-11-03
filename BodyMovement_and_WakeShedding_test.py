fc3e20e46c29e13e6c2e5f1d4b9fe38615c9717d
ichamail <107580530+ichamail@users.noreply.github.com>
2022-11-03 17:57:49 +0200

    update

--- BodyMovement_and_WakeShedding_test.py
+++ BodyMovement_and_WakeShedding_test.py
@@ -33,22 +33,22 @@ wing_mesh.plot_mesh_inertial_frame(elevation=-150, azimuth=-120, plot_wake=False
 wing_mesh.set_body_fixed_frame_origin(xo=0, yo=0, zo=0)
 wing_mesh.set_body_fixed_frame_orientation(roll=0, pitch=0, yaw=0)
 
-omega = Vector((np.deg2rad(0), 0, 0))
+omega = Vector((np.deg2rad(10), 0, 0))
 Vo = Vector((-1, 0, 0))
 V_wind = Vector((0, 0, 0))
 dt = 1
 wing_mesh.set_angular_velocity(omega)
 wing_mesh.set_origin_velocity(Vo)
 
-induced_velocity_function = ( lambda r_p,
-                             body_panels,
-                             wake_panels : Vector((0, 0, -r_p.x * 0.1 )) )
+# induced_velocity_function = ( lambda r_p,
+#                              body_panels,
+#                              wake_panels : Vector((0, 0, -r_p.x * 0.1 )) )
 
 for i in range(5):
     
     wing_mesh.move_body(dt)
     wing_mesh.shed_wake(V_wind, dt)
-    wing_mesh.convect_wake(induced_velocity_function, dt)
+    # wing_mesh.convect_wake(induced_velocity_function, dt)
     # wing_mesh.plot_shells(elevation=-150, azimuth=-120)
     # wing_mesh.plot_panels(elevation=-150, azimuth=-120)
     wing_mesh.plot_mesh_inertial_frame(elevation=-150, azimuth=-120,
