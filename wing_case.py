05b81f255e4cdbbe603208788ca86f1074dde126
ichamail <107580530+ichamail@users.noreply.github.com>
2022-12-15 17:42:00 +0200

    code cleaning

--- wing_case.py
+++ wing_case.py
@@ -5,51 +5,70 @@ from panel_method_class import PanelMethod, Steady_PanelMethod, Trefftz_Plane_An
 from vector_class import Vector
 from matplotlib import pyplot as plt
 from plot_functions import plot_Cp_SurfaceContours
-import time
+from time import perf_counter
 
-# create wing object   
-root_airfoil = Airfoil(name="naca0012_new", chord_length=1)
-tip_airfoil = Airfoil(name="naca0012_new", chord_length=1)
-wing = Wing(root_airfoil, tip_airfoil, semi_span=1, sweep=0, dihedral=0)
+# create wing object
+wing = Wing(
+    root_airfoil=Airfoil(
+        name="naca0012 sharp",
+        chord_length=1
+    ),
+    tip_airfoil=Airfoil(
+        name="naca0012 sharp",
+        chord_length=1
+    ),
+    semi_span=1,
+    sweep=0,
+    dihedral=0
+)
 
 # generate wing mesh
-num_x_bodyShells = 10
-num_x_wakeShells = 1
-num_y_Shells = 10
+nodes, shells, nodes_ids = wing.generate_mesh(
+    num_x_shells=10,
+    num_y_shells=10,
+    num_x_wake_shells=1,
+    mesh_shell_type="quadrilateral",
+    span_wise_spacing="cosine",
+    mesh_main_surface=True,
+    mesh_tips=True,
+    mesh_wake=True, 
+    standard_mesh_format=False
+)
 
-# nodes, shells, nodes_ids = wing.generate_mesh(
-#     num_x_shells=num_x_bodyShells, num_y_shells=num_y_Shells,
-#     mesh_shell_type="quadrilateral",
-#     mesh_main_surface=True, mesh_tips=True, mesh_wake=True, 
-#     num_x_wake_shells=num_x_wakeShells, standard_mesh_format=False 
-# )
-
-# wing_mesh = PanelAeroMesh(nodes, shells, nodes_ids)
-# wing_mesh.plot_mesh_bodyfixed_frame(elevation=-150, azimuth=-120,
-#                                     plot_wake=True)
+wing_mesh = PanelAeroMesh(nodes, shells, nodes_ids)
+wing_mesh.plot_mesh_bodyfixed_frame(
+    elevation=-150, azimuth=-120, plot_wake=True
+)
 
 
 V_fs = Vector((1, 0, 0))
 panel_method = Steady_PanelMethod(V_fs)
-panel_method.set_V_fs(1, AngleOfAttack=2.5, SideslipAngle=0)
+panel_method.set_V_fs(1, AngleOfAttack=0, SideslipAngle=0)
 
-nodes, shells, nodes_ids = wing.generate_mesh2(
-    num_x_shells=num_x_bodyShells, num_y_shells=num_y_Shells,
-    mesh_shell_type="quadrilateral",
-    mesh_main_surface=True, mesh_tips=True, mesh_wake=True, 
-    num_x_wake_shells=num_x_wakeShells, V_fs=panel_method.V_fs,
-    standard_mesh_format=False 
-)
+# # generate wing mesh with wake in the free stream direction
+# nodes, shells, nodes_ids = wing.generate_mesh2(
+#     V_fs=panel_method.V_fs,
+#     num_x_shells=10,
+#     num_y_shells=10,
+#     num_x_wake_shells=1,
+#     mesh_shell_type="quadrilateral",
+#     span_wise_spacing="cosine",
+#     mesh_main_surface=True,
+#     mesh_tips=True,
+#     mesh_wake=True, 
+#     standard_mesh_format=False
+# )
 
-wing_mesh = PanelAeroMesh(nodes, shells, nodes_ids)
-wing_mesh.plot_mesh_bodyfixed_frame(elevation=-150, azimuth=-120,
-                                    plot_wake=True)
+# wing_mesh = PanelAeroMesh(nodes, shells, nodes_ids)
+# wing_mesh.plot_mesh_bodyfixed_frame(
+#     elevation=-150, azimuth=-120, plot_wake=True
+# )
 
-t_start = time.time()        
+
+t_start = perf_counter()        
 panel_method.solve(wing_mesh)
-# panel_method.solve_iteratively(wing_mesh, wing.RefArea, dt=0.2,
-#                                max_iters=2, convergence_value=10**(-3))
-t_end = time.time()
+# panel_method.solve_iteratively(wing_mesh, wing.RefArea, 0.2, 50)
+t_end = perf_counter()
 solution_time = t_end-t_start
 print("solution time = " + str(solution_time))
 
